6d93637ce4c4438ebd4dc6aad7afefafc4274010
Anton Smatanik <smatanik.anton@gmail.com>
2025-12-08 10:40:47 +0100

    formatting

--- client/src/assets/ChevronRight.tsx
+++ client/src/assets/ChevronRight.tsx
@@ -2,9 +2,11 @@ const ChevronRight = () => {
   return (
     <svg
       className="w-4 h-4 text-gray-500"
+      xmlns="http://www.w3.org/2000/svg"
       fill="none"
       stroke="currentColor"
       viewBox="0 0 24 24"
+      aria-hidden="true"
     >
       <path
         strokeLinecap="round"
